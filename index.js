7869d8696ad58b2aa0d09ba69ed291ba6a27635e
Daniel Biedma <info@dbram.co>
2015-10-10 17:45:58 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+var Client = require('mongodb').MongoClient,
+  mongodbUri = require('mongodb-uri');
+
+var config = {
+  hosts: [
+    {
+      host: '192.168.99.100',
+      port: 32771
+    }
+  ],
+  database: 'ada'
+};
+
+Client.connect(mongodbUri.format(config), {'uri_decode_auth': true}, function(err, db) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    continuacion(db);
+});
+
+var continuacion =  function (db){
+    var collection = db.collection('articulos');
+    collection.insert({a: 1}, function(err, result) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        console.log('OK');
+        db.close();
+        return;
+    });
+}
